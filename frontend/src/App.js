77de6ce9016ef73a2713e04222fb32aa97668d47
mguazzardo <mguazzardo76@gmail.com>
2021-10-14 21:39:54 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,25 @@
-import React, { Component } from 'react';
-import { Container, Header, Message, Table } from 'semantic-ui-react';
+import React from 'react';
 
-export default class App extends Component {
+import axios from 'axios';
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      topics: null,
-      isLoading: null
-    };
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
   }
 
-  componentWillMount() {
-    this.getTopics();
-
-
-  async getTopics = () => {
-
-    try {
-
-      this.setState({ isLoading: true });
-      let response = await fetch('http://backend:8080/api/topics');
-      let data = await response.json();
-
-      this.setState({
-        topics: data,
-        isLoading: false
-      });
-
-    } catch (error) {
-      this.setState({ isLoading: false });
-      console.error(error);
-    }
-
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
   }
 
   render() {
     return (
-      <Container text style={{ marginTop: '7em' }}>
-        <Header as="h1">Topics</Header>
-        {this.state.isLoading && <Message info header="Loading topics..." />}
-        {this.state.topics && (
-          <div>
-            <Table>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.topics.map(topic => (
-                  <tr id={topic._id} key={topic._id}>
-                    <td>{topic.name}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </div>
-        )}
-      </Container>
-    );
+      <ul>
+        { this.state.persons.map(person => <li>{person.name}</li>)}
+      </ul>
+    )
   }
 }
